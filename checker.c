2db0ea5b4245ec7446cd7bc16b4858e7f12585ee
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-12-09 19:53:08 +0530

    Update checker to reduce complexity

--- checker.c
+++ checker.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-#include <assert.h>
+#include "checker_functions.h"
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    printf("Temperature out of range!\n");
-    return 0;
-  } else if(soc < 20 || soc > 80) {
-    printf("State of Charge out of range!\n");
-    return 0;
-  } else if(chargeRate > 0.8) {
-    printf("Charge Rate out of range!\n");
-    return 0;
+int batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+  int temperature_ret_val = checkTemperatureRange(temperature);
+  int soc_ret_val = checkSocRange(soc);
+  int charge_rate_ret_val = checkChargeRateRange(chargeRate);
+
+  if(temperature_ret_val == 1 && soc_ret_val == 1 && charge_rate_ret_val == 1)
+  {
+    return 1;
   }
-  return 1;
-}
 
-int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+  return 0;
 }
