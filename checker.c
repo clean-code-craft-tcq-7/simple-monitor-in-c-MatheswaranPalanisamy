1c262c2658a9831e578a5ce796f7399573cdab42
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-12-09 20:08:45 +0530

    Update checker for addtional conditions

--- checker.c
+++ checker.c
@@ -3,9 +3,13 @@
 
 int batteryIsOk(float temperature, float soc, float chargeRate) 
 {
-  int temperature_ret_val = checkTemperatureRange(temperature);
-  int soc_ret_val = checkSocRange(soc);
-  int charge_rate_ret_val = checkChargeRateRange(chargeRate);
+  if(checkTemperatureRange(temperature))
+  {
+    if(checkSocRange(soc))
+    {
+      return checkChargeRateRange(chargeRate);
+    }
+  }
 
-  return temperature_ret_val & soc_ret_val & charge_rate_ret_val;
+  return 0;
 }
