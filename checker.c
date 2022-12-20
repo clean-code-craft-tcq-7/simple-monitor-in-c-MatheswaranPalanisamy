be1b6370dfb55f88f52f1a4e3d6514499b579967
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-12-20 11:57:58 +0530

    Update checker for database for each parameters

--- checker.c
+++ checker.c
@@ -5,14 +5,29 @@
 #include "checker_functions.h"
 #include "checker.h"
 
-bmsChecker_st checkerDatabase[BMS_MAX_PARAMTERS];
+bmsChecker_st checkerDatabase[BMS_MAX_PARAMTERS] = {
+  {
+    .limits = (limits_st *)&Bms_limitsDataBase[BMS_TEMPERATURE],
+    .language = ENGLISH,
+    .enableWarning = 1,
+  },
+  {
+    .limits = (limits_st *)&Bms_limitsDataBase[BMS_SOC],
+    .language = ENGLISH,
+    .enableWarning = 1,
+  },
+  {
+    .limits = (limits_st *)&Bms_limitsDataBase[BMS_CHARGE_RATE],
+    .language = ENGLISH,
+    .enableWarning = 1,
+  }
+};
 
 int checkerInit(languages_en language, int *warningEnable)
 {
   int paramCount = 0;
   for(paramCount = 0; paramCount < BMS_MAX_PARAMTERS; paramCount++)
   {
-    checkerDatabase[paramCount].limits = (limits_st*)Bms_limitsDataBase[paramCount];
     // select Language
     checkerDatabase[paramCount].language = language;
     // Enable or disable warning  levels
@@ -24,10 +39,8 @@ int checkerInit(languages_en language, int *warningEnable)
 
 int batteryIsOk(float temperature, float soc, float chargeRate, void (*alerter)(limitCategory_en, char *)) 
 {
-  //int return_code = 0;
   float parameterArray[BMS_MAX_PARAMTERS] = {temperature, soc, chargeRate};
-  printf("Inputs %f, %f, %f\n", temperature, soc, chargeRate);
+  printf("Inputs : %f, %f, %f\n", temperature, soc, chargeRate);
+ 
   return checkAndAlertParameters(parameterArray, alerter);
-  //return_code = checkTemperatureRange(temperature) & checkSocRange(soc) & checkChargeRateRange(chargeRate);
-  //return return_code;
 }
