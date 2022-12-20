3bc1bc0bf826b3109293d06b2643cf3c66ac384a
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-12-20 11:37:11 +0530

    Update main for extended features

--- main.c
+++ main.c
@@ -1,28 +1,41 @@
 #include <assert.h>
+#include "bms_common.h"
 #include "checker.h"
 #include "checker_functions.h"
+#include "bms_io.h"
 
 int main() {
+  int enableWarning[BMS_MAX_PARAMTERS] = {1, 1, 1};
+  assert(checkerInit(GERMAN, enableWarning) == 0);
+  
   // Test batterOk for success
-  assert(batteryIsOk(25, 70, 0.7) == 1);
+  //assert(batteryIsOk(25, 70, 0.7, printError) == 1);
 
+  assert(batteryIsOk(-3.5, 43, 0.4, printMessage) == 0);
+  assert(batteryIsOk(0, 70, 0.7, printMessage) == 1);
+  assert(batteryIsOk(1.0, 70, 0.7, printMessage) == 1);
+  assert(batteryIsOk(5.0, 70, 0.7, printMessage) == 1);
+  assert(batteryIsOk(40.0, 70, 0.7, printMessage) == 1);
+  assert(batteryIsOk(44.9, 70, 0.7, printMessage) == 1);
+  assert(batteryIsOk(45.0, 70, 0.7, printMessage) == 0);
+  assert(batteryIsOk(47.0, 70, 0.7, printMessage) == 0);
   // Test batterOk for temperature failure, soc fail and chargeRate fail
-  assert(batteryIsOk(50, 90, 0.9) == 0);
+  //assert(batteryIsOk(3, 70, 0.7, printError) == 1);
   // Test batterOk for temperature failure, soc fail and chargeRate success
-  assert(batteryIsOk(50, 88, 0.3) == 0);
+ // assert(batteryIsOk(50, 88, 0.3, printError) == 0);
   // Test batterOk for temperature failure, soc success and chargeRate fail
-  assert(batteryIsOk(50, 40, 1.2) == 0);
-// Test batterOk for temperature failure, soc success and chargeRate success
-  assert(batteryIsOk(50, 32, 0.6) == 0);
+ // assert(batteryIsOk(50, 40, 1.2, printError) == 0);
+  // Test batterOk for temperature failure, soc success and chargeRate success
+ // assert(batteryIsOk(50, 32, 0.6, printError) == 0);
 
   // Test batterOk for temperature success, soc fail and chargeRate fail
-  assert(batteryIsOk(30, 85, 1.8) == 0);
+  //assert(batteryIsOk(30, 85, 1.8, printError) == 0);
   // Test batterOk for temperature success, soc fail and chargeRate success
-  assert(batteryIsOk(30, 85, 0.4) == 0);
+ // assert(batteryIsOk(30, 85, 0.4, printError) == 0);
 
   // Test batterOk for temperature and soc success, chargeRate fail
-  assert(batteryIsOk(30, 43, 1) == 0);
-
+ // assert(batteryIsOk(30, 43, 1, printError) == 0);
+/*
   // Test temperature range for out of lower boundary
   assert(checkTemperatureRange(-0.1) == 0);
   // Test temperature range for lower boundary
@@ -61,6 +74,6 @@ int main() {
   assert(checkChargeRateRange(0.8) == 1);
   // Test chargerRate range for out of boundary
   assert(checkChargeRateRange(0.9) == 0);
-
+*/
   return 0;
 }
