32dbc39c08c4b8119aa54382c79a44a9bef0ae31
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-12-20 20:13:43 +0530

    Remove unused code

--- main.c
+++ main.c
@@ -139,43 +139,5 @@ int main() {
   testBmsChecker(6, 25, 0.81, printMessage, fillExpectedOutput(0, "", "", "Gebührenrate liegt über dem oberen Limit!", 0, 0, 2));
   testBmsChecker(6, 25, 0.85, printMessage, fillExpectedOutput(0, "", "", "Gebührenrate liegt über dem oberen Limit!", 0, 0, 2));
   
-/*
-
-  assert(checkerInit(GERMAN, enableWarning) == 0);
-  
-  // Test batterOk for success
-  assert(batteryIsOk(25, 70, 0.7, printMessage) == 1);
-  assert(batteryIsOk(-3.5, 43, 0.4, printMessage) == 0);
-  assert(batteryIsOk(0, 70, 0.7, printMessage) == 1);
-  assert(batteryIsOk(0.1, 70, 0.7, printMessage) == 1);
-  assert(batteryIsOk(5.0, 70, 0.7, printMessage) == 1);
-  assert(batteryIsOk(40.0, 70, 0.7, printMessage) == 1);
-  assert(batteryIsOk(44.9, 70, 0.7, printMessage) == 1);
-  assert(batteryIsOk(45.0, 70, 0.7, printMessage) == 0);
-  assert(batteryIsOk(47.0, 70, 0.7, printMessage) == 0);
-
-  // Test batterOk for success
-  assert(batteryIsOk(25, -10, 0.7, printMessage) == 0);
-  assert(batteryIsOk(2, 19.9, 0.4, printMessage) == 0);
-  assert(batteryIsOk(45, 20, 0.7, printMessage) == 0);
-  assert(batteryIsOk(0.1, 20.1, 0.7, printMessage) == 1);
-  assert(batteryIsOk(5.0, 24.9, 0.7, printMessage) == 1);
-  assert(batteryIsOk(40.0, 25, 0.7, printMessage) == 1);
-  assert(batteryIsOk(44.9, 25.1, 0.7, printMessage) == 1);
-  assert(batteryIsOk(40, 74.9, 0.7, printMessage) == 1);
-  assert(batteryIsOk(0.7, 75, 0.7, printMessage) == 1);
-  assert(batteryIsOk(40, 75.1, 0.7, printMessage) == 1);
-  assert(batteryIsOk(0.7, 79.9, 0.7, printMessage) == 1);
-  assert(batteryIsOk(0.7, 80, 0.7, printMessage) == 0);
-  assert(batteryIsOk(0.7, 80.1, 0.7, printMessage) == 0);
-
-  assert(batteryIsOk(25, 21, -0.7, printMessage) == 1);
-  assert(batteryIsOk(2, 22, 0.74, printMessage) == 1);
-  assert(batteryIsOk(42, 20.2, 0.75, printMessage) == 1);
-  assert(batteryIsOk(0.1, 20.1, 0.76, printMessage) == 1);
-  assert(batteryIsOk(5.0, 24.9, 0.79, printMessage) == 1);
-  assert(batteryIsOk(40.0, 25, 0.8, printMessage) == 0);
-  assert(batteryIsOk(44.9, 25.1, 0.81, printMessage) == 0);
-  assert(batteryIsOk(40, 74.9, 0.85, printMessage) == 0); */
   return 0;
 }
