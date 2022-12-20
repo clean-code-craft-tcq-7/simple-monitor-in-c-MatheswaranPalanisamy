be2f14b5ee3d78b389bf379ca5a87a0bb4f71c45
MatheswaranPalanisamy <mathespbe@gmail.com>
2022-12-20 11:02:09 +0530

    Update checker to modify init and batteryOk interfaces

--- checker.c
+++ checker.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
-#include "checker_functions.h"
 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
+#include "bms_common.h"
+#include "bms_limits.h"
+#include "checker_functions.h"
+#include "checker.h"
+
+bmsChecker_st checkerDatabase[BMS_MAX_PARAMTERS];
+
+int checkerInit(languages_en language, int *warningEnable)
 {
-  int return_code = 0;
-  return_code = checkTemperatureRange(temperature) & checkSocRange(soc) & checkChargeRateRange(chargeRate);
-  return return_code;
+  int paramCount = 0;
+  for(paramCount = 0; paramCount < BMS_MAX_PARAMTERS; paramCount++)
+  {
+    checkerDatabase[paramCount].limits = (limits_st*)Bms_limitsDataBase[paramCount];
+    // select Language
+    checkerDatabase[paramCount].language = language;
+    // Enable or disable warning  levels
+    checkerDatabase[paramCount].enableWarning = warningEnable[paramCount];
+  }
+
+  return 0;
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate, void (*alerter)(limitCategory_en, char *)) 
+{
+  //int return_code = 0;
+  float parameterArray[BMS_MAX_PARAMTERS] = {temperature, soc, chargeRate};
+
+  return checkAndAlertParameters(parameterArray, alerter);
+  //return_code = checkTemperatureRange(temperature) & checkSocRange(soc) & checkChargeRateRange(chargeRate);
+  //return return_code;
 }
--- checker.h
+++ checker.h
@@ -1,1 +1,17 @@
-int batteryIsOk(float temperature, float soc, float chargeRate) ;
+#ifndef BMS_CHECKER_H
+#define BMS_CHECKER_H
+
+typedef struct
+{
+    limits_st *limits;
+    int enableWarning;
+    languages_en language;
+
+}bmsChecker_st;
+
+int batteryIsOk(float temperature, float soc, float chargeRate, void (*alerter)(limitCategory_en, char *));
+int checkerInit(languages_en language, int *warningEnable);
+
+extern bmsChecker_st checkerDatabase[BMS_MAX_PARAMTERS];
+
+#endif
